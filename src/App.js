e46047940e1d39366f553dd263d9cf914874269b
Rachata Charoensuk [TH] <RCharoensuk@kerrylogistics.com>
2019-07-26 14:08:05 +0700

    Clear UI

--- src/App.js
+++ src/App.js
@@ -7,21 +7,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
